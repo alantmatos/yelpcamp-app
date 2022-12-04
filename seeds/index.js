b4d135ebbc0abb2eb374afa8599132ae287b3da7
Alan Torquatro de Matos <mr.alanmatos@hotmail.com>
2022-12-04 16:03:44 -0500

    applied basic styles to all pages, crud features working, updated database

--- seeds/index.js
+++ seeds/index.js
@@ -22,9 +22,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for ( let i = 0; i < 50; i++){
         const randomcity = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random()*50) + 10;
         const camp = new Campground({
             location: `${cities[randomcity].city}, ${cities[randomcity].state}}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero quae explicabo impedit dicta expedita maxime incidunt. Debitis, fugiat omnis, ab cupiditate quis blanditiis, magnam voluptas sequi similique rerum repudiandae at!",
+            price: price
         })
         await camp.save();
     }
