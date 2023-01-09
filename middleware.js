9f88dbd5a202d80ca15673bbd59b93c78bfad67e
alantmatos <mr.alanmatos@hotmail.com>
2023-01-09 10:49:39 -0500

    middleware refactored

--- middleware.js
+++ middleware.js
@@ -1,3 +1,8 @@
+const { campgroundSchema, reviewSchema } = require('./schemas.js');
+const ExpressError = require('./utilities/ExpressError');
+const Campground = require('./models/campground');
+const passport = require('passport');
+
 
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
@@ -6,4 +11,28 @@ module.exports.isLoggedIn = (req, res, next) => {
         return res.redirect('/login');
     }
     next();
+};
+
+
+
+module.exports.validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const errorMsg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(errorMsg, 400)
+    } else {
+        next();
+    }
+};
+
+
+module.exports.isAuthor = async ( req, res, next) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    if(!campground.author.equals(req.user._id)){
+        req.flash('error', 'You do not have permission to do that');
+        return res.redirect(`/campgrounds/${id}`)
+    }else{
+        next();
+    }
 };
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,34 +2,8 @@ const express = require('express');
 const router = express.Router();
 const Campground = require('../models/campground');
 const CatchAsync = require('../utilities/CatchAsync');
-const { campgroundSchema, reviewSchema } = require('../schemas.js');
-const ExpressError = require('../utilities/ExpressError');
 const passport = require('passport');
-const { isLoggedIn } = require('../middleware');
-
-
-
-const validateCampground = (req, res, next) => {
-    const { error } = campgroundSchema.validate(req.body);
-    if (error) {
-        const errorMsg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(errorMsg, 400)
-    } else {
-        next();
-    }
-}
-
-
-const isAuthor = async ( req, res, next) => {
-    const { id } = req.params;
-    const campground = await Campground.findById(id);
-    if(!campground.author.equals(req.user._id)){
-        req.flash('error', 'You do not have permission to do that');
-        return res.redirect(`/campgrounds/${id}`)
-    }else{
-        next();
-    }
-}
+const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 
 
 
@@ -39,7 +13,7 @@ router.get('/', CatchAsync(async (req, res, next) => {
     res.render('campgrounds/index', { campgrounds })
 }));
 
-router.delete('/:id', isLoggedIn, isAuthor, CatchAsync(async (req, res, next) => {
+router.delete('/:id', isLoggedIn, CatchAsync(async (req, res, next) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndDelete(id);
     req.flash('success', 'The campground was successfully deleted');
@@ -60,6 +34,7 @@ router.get('/new', isLoggedIn, (req, res) => {
 router.post('/', isLoggedIn, validateCampground, CatchAsync(async (req, res, next) => {
     // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400)
     const campground = new Campground(req.body.campground);
+    campground.author = req.user._id;
     await campground.save();
     req.flash('success', 'The new campground was successfully created!');
     res.redirect(`/campgrounds/${campground._id}`)
