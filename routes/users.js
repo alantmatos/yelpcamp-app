30608684c33c916e549766983b4d00884ec15044
alantmatos <mr.alanmatos@hotmail.com>
2023-01-08 22:13:04 -0500

    implementation of req.originalUrl and redirect upon login

--- routes/users.js
+++ routes/users.js
@@ -16,8 +16,12 @@ router.post('/register', CatchAsync(async (req, res) => {
         const { username, email, password } = req.body.user;
         const user = new User({ username, email });
         const registeredUser = await User.register(user, password);
-        req.flash('success', "Welcome to YelpCamp!")
-        res.redirect('/campgrounds');
+        req.login(registeredUser, err => {
+            if(err) return next(err);
+            req.flash('success', "Welcome to YelpCamp!")
+            res.redirect('/campgrounds');
+        })
+        
     } catch (e) {
         req.flash('error', e.message)
         res.redirect('/register');
@@ -38,9 +42,14 @@ router.get('/logout', (req, res) => {
     });
 });
 
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res, next) => {
     req.flash('success', 'Welcome Back!');
-    res.redirect('/campgrounds');
+    //saves url user tried to acess, redirect after loguedin/
+    //the session is getting lost upon loggin, losing originalurl
+    const redirectUserTo = req.session.returnTo || '/campgrounds';
+    console.log('url to return is ', redirectUserTo);
+    res.redirect(redirectUserTo);
+    //delete req.session.returnTo;
 });
 
 module.exports = router;
